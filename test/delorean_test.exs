90ec5b1d3818d0a2ba93f41cdbe56016d5c1ce09
Will Lewis <will@wflewis.com>
2016-08-25 09:58:17 -0400

    Add another test and clean up the old tests

--- test/delorean_test.exs
+++ test/delorean_test.exs
@@ -3,38 +3,37 @@ defmodule DeloreanTest do
   doctest Delorean
 
   test "that we can go to yesterday" do
-    today = ~D[2016-08-25]
-    yesterday = ~D[2016-08-24]
-    assert yesterday == Delorean.travel!(today, day: -1)
+    assert ~D[1985-10-20] == Delorean.travel!(today, day: -1)
   end
 
   test "that we can go to a month ago" do
-    today = ~D[2016-08-25]
-    last_month = ~D[2016-07-25]
-    assert last_month == Delorean.travel!(today, month: -1)
+    assert ~D[1985-09-21] == Delorean.travel!(today, month: -1)
   end
 
   test "that we can go to a year ago" do
-    today = ~D[2016-08-25]
-    last_year = ~D[2015-08-25]
-    assert last_year == Delorean.travel!(today, year: -1)
+    assert ~D[1984-10-21] == Delorean.travel!(today, year: -1)
   end
 
   test "that we can go to tomorrow" do
-    today = ~D[2016-08-25]
-    tomorrow = ~D[2016-08-26]
-    assert tomorrow == Delorean.travel!(today, day: 1)
+    assert ~D[1985-10-22] == Delorean.travel!(today, day: 1)
   end
 
   test "that we can go to a month from now" do
-    today = ~D[2016-08-25]
-    next_month = ~D[2016-09-25]
-    assert next_month == Delorean.travel!(today, month: 1)
+    assert ~D[1985-11-21] == Delorean.travel!(today, month: 1)
   end
 
   test "that we can go to a year from now" do
-    today = ~D[2016-08-25]
-    next_year = ~D[2017-08-25]
-    assert next_year == Delorean.travel!(today, year: 1)
+    assert ~D[1986-10-21] == Delorean.travel!(today, year: 1)
+  end
+
+  test "that we can go to the past" do
+    assert ~D[1955-11-12] == today
+    |> Delorean.travel!(year: -30)
+    |> Delorean.travel!(month: 1)
+    |> Delorean.travel!(day: -9)
+  end
+
+  def today do
+    ~D[1985-10-21]
   end
 end
