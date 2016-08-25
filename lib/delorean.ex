5992b46679649550e4d8f8e277dbd41b6b9bb15f
Will Lewis <will@wflewis.com>
2016-08-25 14:29:12 -0400

    Add a method for comparing dates

--- lib/delorean.ex
+++ lib/delorean.ex
@@ -15,4 +15,33 @@ defmodule Delorean do
     {:ok, destination} = travel(present, opts)
     destination
   end
+
+  def compare!(date1, date2) do
+    year? = compare_map_key(date1, date2, :year)
+    if year? == 0 do
+      month? = compare_map_key(date1, date2, :month)
+      if month? == 0 do
+        day? = compare_map_key(date1, date2, :day)
+        if day? == 0 do
+          0
+        else
+          day?
+        end
+      else
+        month?
+      end
+    else
+      year?
+    end
+  end
+
+  def compare_map_key(item1, item2, key) do
+    a = Map.get(item1, key)
+    b = Map.get(item2, key)
+    cond do
+      a == b -> 0
+      a > b -> 1
+      a < b -> -1
+    end
+  end
 end
